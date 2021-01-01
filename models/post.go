d5fcf757d3e9d4159af5eed763940fe4b7467c2c
Robert Hawk <robert@the-hawk.us>
2021-01-01 00:23:04 -0700

    Added language field and validation to model
    Will still need to update the database.

--- models/post.go
+++ models/post.go
@@ -1,12 +1,38 @@
 package models
 
-import "time"
+import (
+	iso639 "github.com/emvi/iso-639-1"
+	"github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
+	"time"
+)
 
 type Post struct {
 	BaseModelWithSoftDelete
 	Slug        string    `json:"slug"`
 	Title       string    `json:"title"`
 	PublishDate time.Time `json:"publish_date"`
+	Language    string    `json:"language"`
 	Body        string    `json:"body"`
 	Authors     []Author  `json:"authors" gorm:"many2many:author_posts"`
+}
+
+func verifyISO639(code interface{}) error {
+	c, ok := code.(string)
+	if !ok {
+		return errors.Errorf("%v is not a valid ISO-639-1 code", code)
+	}
+
+	if !iso639.ValidCode(c) {
+		return errors.Errorf("%s is not a valid ISO-639-1 code", code)
+	}
+
+	return nil
+}
+
+func (p *Post) BeforeSave(tx *gorm.DB) error {
+	return validation.ValidateStruct(p,
+		validation.Field(&p.Language, validation.Required, validation.By(verifyISO639)),
+	)
 }
